ee8f7c4d4df67169e8023578a258c698e366599d
Radford Smith <11401+rads@users.noreply.github.com>
2022-07-30 09:35:35 -0700

    Improve CLI parsing and error handling

--- src/rads/preload.clj
+++ src/rads/preload.clj
@@ -8,12 +8,25 @@
       (-> (edn/read-string (slurp file))
           (get-in [:aliases :preload ::namespaces])))))
 
+(defn warn [& messages]
+  (apply println "WARNING: rads.preload:" messages))
+
+(defn handle-error [error preload-ns]
+  (warn "Failed to require namespace:" preload-ns)
+  (warn "Are you calling clj/clojure with the :preload alias?")
+  (warn "Error output:")
+  (println)
+  (println error)
+  (println))
+
 (defn -main [& main-args]
-  (let [target-ns (symbol (nth main-args 1))
-        wrapped-fn #(resolve (symbol (str target-ns) "-main"))
-        wrapped-args (drop 2 main-args)
+  (let [split-args (split-with #(not= % "-m") main-args)
+        [_ [_ target-ns & target-args]] split-args
         namespaces (load-namespaces)]
     (doseq [preload-ns namespaces]
-      (require preload-ns))
-    (require target-ns)
-    (apply (wrapped-fn) wrapped-args)))
+      (try
+        (require preload-ns)
+        (catch Throwable e
+          (handle-error e preload-ns))))
+    (apply (requiring-resolve (symbol (str target-ns) "-main"))
+           target-args)))
